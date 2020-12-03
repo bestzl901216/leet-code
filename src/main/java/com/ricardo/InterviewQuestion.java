ba80110f2a8bea0cab096c52229c85f201fb28b5
zhoulin <670919976@qq.com>
2020-12-03 22:37:46 +0800

    力扣-名企高频面试题 1

---
+++ src/main/java/com/ricardo/InterviewQuestion.java
@@ -0,0 +1,42 @@
+package com.ricardo;
+
+/**
+ * 力扣-名企高频面试题
+ *
+ * @author Ricardo
+ * @date 2020/12/1
+ */
+public class InterviewQuestion {
+
+    /**
+     * 最大子序和
+     * 给定一个整数数组 nums ，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+     * <p>
+     * 示例:
+     * <p>
+     * 输入: [-2,1,-3,4,-1,2,1,-5,4]
+     * 输出: 6
+     * 解释: 连续子数组 [4,-1,2,1] 的和最大，为 6。
+     * 进阶:
+     * <p>
+     * 如果你已经实现复杂度为 O(n) 的解法，尝试使用更为精妙的分治法求解。
+     * <p>
+     * 作者：力扣 (LeetCode)
+     * 链接：https://leetcode-cn.com/leetbook/read/2020-top-interview-questions/xo341d/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     *
+     * @param nums 整数数组
+     * @return 最大和
+     */
+    public int maxSubArray(int[] nums) {
+        int[] dp = new int[nums.length];
+        int max = nums[0];
+        dp[0] = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            dp[i] = Math.max(nums[i] + dp[i - 1], nums[i]);
+            max = Math.max(max, dp[i]);
+        }
+        return max;
+    }
+}
